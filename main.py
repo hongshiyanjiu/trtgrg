3df58426bab3623cbb2459a64049be11f07e7bb7
Leo Kim <me@leok.kr>
2021-09-25 02:17:42 +0900

    fix: print whole json

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ url = "https://hk4e-api-os.mihoyo.com/event/sol/sign?act_id=e202102251931481&lan
 cookies = {'ltuid': ltuid, 'ltoken': ltoken}
 
 r = requests.post(url, cookies=cookies)
-print(r.json()['message'])
+print(r.json())
